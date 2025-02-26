8fb130ef6e9c8e366085b19b04c6927ec07f7ec0
manellylopez <manellylopezqqq@gmail.com>
2025-02-26 16:26:06 -0500

    load homepage content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+let didInit = false;
+const titleDiv = document.querySelector("title");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialData = {};
+  const initialTitle = "Blog API";
+  const [blogdata, setBlogdata] = useState(initialData);
+  titleDiv.textContent =
+    blogdata.title === undefined ? initialTitle : blogdata.title;
+
+  useEffect(() => {
+    if (!didInit) {
+      didInit = true;
+      // ✅ Only runs once per app load
+      getData("https://top-backend-blogapi.onrender.com/");
+    }
+  });
+
+  async function getData(arg) {
+    try {
+      const response = await fetch(arg, { mode: "cors" });
+      const responseData = await response.json();
+      setBlogdata(responseData);
+    } catch (error) {
+      alert("Something was wrong. try again later");
+      console.log(error);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="blog-content">
+      <p >nuevo proyecto</p>
+
+      <p> {blogdata.message === undefined ? null : blogdata.message} </p>
+
+      {!blogdata.allPosts ? (
+        <div>Loading...</div>
+      ) : blogdata.allPosts.length > 0 ? (
+        <ul>
+          {blogdata.allPosts.map((post) => {
+            return <li key={post.id}>
+              <p>{post.title}</p>
+              <p>{post.authorId}</p>
+              
+              <button>{post.published===true ? "published" : "unpublished"}</button>
+              <button>EDIT</button>
+              </li>;
+          })}
+        </ul>
+      ) : (
+        <div>There are no posts published!</div>
+      )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
