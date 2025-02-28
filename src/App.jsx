73810438aeca3a6c63548846657e78b7234d020a
manellylopez <manellylopezqqq@gmail.com>
2025-02-28 16:19:24 -0500

    modify useEffect only on mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "./App.css";
 
-let didInit = false;
 const titleDiv = document.querySelector("title");
 
 function App() {
   const initialData = {};
   const initialTitle = "Blog API";
   const [blogdata, setBlogdata] = useState(initialData);
-  titleDiv.textContent =
-    blogdata.title === undefined ? initialTitle : blogdata.title;
+
+  if (titleDiv) {
+    blogdata.title === undefined
+      ? (titleDiv.textContent = initialTitle)
+      : (titleDiv.textContent = blogdata.title);
+  }
 
   useEffect(() => {
-    if (!didInit) {
-      didInit = true;
-      // ✅ Only runs once per app load
-      getData("https://top-backend-blogapi.onrender.com/");
-    }
-  });
+    getData("https://top-backend-blogapi.onrender.com/");
+  }, []);
 
   async function getData(arg) {
     try {
       const response = await fetch(arg, { mode: "cors" });
       const responseData = await response.json();
       setBlogdata(responseData);
+      return setBlogdata;
     } catch (error) {
       alert("Something was wrong. try again later");
       console.log(error);
@@ -36,7 +36,9 @@ function App() {
     <>
       <nav>
         <div>
-          <Link to="">SIGN UP</Link>
+          <Link to="sign_up" >
+            SIGN UP
+          </Link>
         </div>
         <div>
           <Link to="">LOGIN</Link>
@@ -44,27 +46,32 @@ function App() {
       </nav>
 
       <div className="blog-content">
-      <h2 >website for authoring and editing posts</h2>
+        <h2>website for authoring and editing posts</h2>
 
-      <p> {blogdata.message === undefined ? null : blogdata.message} </p>
+        <p> {blogdata.message === undefined ? null : blogdata.message} </p>
 
-      {!blogdata.allPosts ? (
-        <div>Loading...</div>
-      ) : blogdata.allPosts.length > 0 ? (
-        <ul>
-          {blogdata.allPosts.map((post) => {
-            return <li key={post.id}>
-              <p>{post.title}</p>
-              <p>{post.authorId}</p>
-              
-              <button>{post.published===true ? "published" : "unpublished"}</button>
-              <button>EDIT</button>
-              </li>;
-          })}
-        </ul>
-      ) : (
-        <div>There are no posts published!</div>
-      )}
+        {!blogdata.allPosts ? (
+          <div>Loading...</div>
+        ) : blogdata.allPosts.length > 0 ? (
+          <ul>
+            {blogdata.allPosts.map((post) => {
+              return (
+                <li key={post.id}>
+                  <p>{post.title}</p>
+                  <p>{post.authorId}</p>
+
+                  <button>
+                    {post.published === true ? "published" : "unpublished"}
+                  </button>
+                  <button>EDIT</button>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <div>There are no posts published!</div>
+        )}
+       
       </div>
     </>
   );
