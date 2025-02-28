f43b875252865b5e718f0d9be8e3d5045d66ef16
manellylopez <manellylopezqqq@gmail.com>
2025-02-28 16:26:05 -0500

    routes component

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,10 +1,18 @@
 import App from "../App";
+import ErrorPage from './error_page';
+import SignUp from './SignUp';
 
 const routes = [
   {
     index: true,
     path: "/",
     element: <App />,
+    
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'sign_up',
+    element: <SignUp />,
   },
 ];
 
