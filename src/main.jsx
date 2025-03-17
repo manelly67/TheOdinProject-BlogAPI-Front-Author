5460bd6d7f00c506fe41130b88a5108c121b24f8
manellylopez <manellylopezqqq@gmail.com>
2025-03-17 15:06:04 -0500

    modify duplicated meta description

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import routes from "./components/routes.jsx";
 
-const meta = document.createElement('meta');
-meta.setAttribute('meta','description');
-meta.setAttribute('content','manelly67\'s exercise study project:BLOG API within the curriculum The Odin Project');
-document.head.appendChild(meta);
 
 const router = createBrowserRouter(routes);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
